90f3ec836e9248d68756039e1aff556a3cb445d2
CooperHeriot <chepic9990@gmail.com>
2023-10-17 13:04:55 +1100

    fd

---
+++ Assets/Scripts/PlayerScripts/LaserPointer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    public GameObject Mark, firepoint;
+
+    public LayerMask LM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(firepoint.transform.position, firepoint.transform.forward, out hit, 80, LM))
+        {
+
+            Mark.SetActive(true);
+
+            Mark.transform.position = hit.point;
+        } else
+        {
+            Mark.SetActive(false);
+        }
+    }
+}
