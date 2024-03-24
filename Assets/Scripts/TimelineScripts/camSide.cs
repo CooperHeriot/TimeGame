0953319ab0aa65ea9987f3ba97ccd519f5b59b2b
CooperHeriot <chepic9990@gmail.com>
2024-03-24 23:46:04 +1100

    ada

---
+++ Assets/Scripts/TimelineScripts/camSide.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSide : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void left()
+    {
+
+    }
+
+    public void right()
+    {
+
+    }
+
+    public void iddle()
+    {
+
+    }
+}
