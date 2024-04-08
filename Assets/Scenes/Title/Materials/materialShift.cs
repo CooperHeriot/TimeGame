edf9a8eafeff8599a8dd27182ae5b0a83c2013fc
CooperHeriot <107088336+CooperHeriot@users.noreply.github.com>
2024-04-08 15:56:24 +1000

    title

---
+++ Assets/Scenes/Title/Materials/materialShift.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class materialShift : MonoBehaviour
+{
+    public Material ren;
+
+    public float thinger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ren.SetTextureOffset("_MainTex",new Vector2(0, thinger * Time.deltaTime));
+        ren.mainTextureOffset += new Vector2(0, thinger * Time.deltaTime);
+    }
+}
